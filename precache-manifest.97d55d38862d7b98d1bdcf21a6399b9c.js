c1726699091071476467b71843cf265cece164ac
Talha-Jawed <jawedtalha0@gmail.com>
2020-06-21 15:28:26 +0500

    Updates

---
+++ precache-manifest.97d55d38862d7b98d1bdcf21a6399b9c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0317bcd1640170ea39b83b966fbd038",
+    "url": "/Al-Madina/index.html"
+  },
+  {
+    "revision": "e92b14c6e1b1318ea2f0",
+    "url": "/Al-Madina/static/css/main.dd08fd63.chunk.css"
+  },
+  {
+    "revision": "ab968b5cd2eafd4fe06f",
+    "url": "/Al-Madina/static/js/2.f16435fc.chunk.js"
+  },
+  {
+    "revision": "e92b14c6e1b1318ea2f0",
+    "url": "/Al-Madina/static/js/main.64c5d572.chunk.js"
+  },
+  {
+    "revision": "d3117c42715df50c7a95",
+    "url": "/Al-Madina/static/js/runtime-main.5982846e.js"
+  },
+  {
+    "revision": "8667d47626357c512fdf743c7ad08be0",
+    "url": "/Al-Madina/static/media/batch.8667d476.jpeg"
+  },
+  {
+    "revision": "9812fa9637a59c4dfc467dac2c331752",
+    "url": "/Al-Madina/static/media/bg.9812fa96.jpg"
+  },
+  {
+    "revision": "4921546f460419823d1d54b286d76e17",
+    "url": "/Al-Madina/static/media/carpigiani.4921546f.jpeg"
+  },
+  {
+    "revision": "369cee7c217c815b62aa3916463f82c9",
+    "url": "/Al-Madina/static/media/closeWhite.369cee7c.png"
+  },
+  {
+    "revision": "e82e21e54417d2d06ea24d3ea270694f",
+    "url": "/Al-Madina/static/media/devTo.e82e21e5.png"
+  },
+  {
+    "revision": "e953d4a1660c385ba5b0443419f68208",
+    "url": "/Al-Madina/static/media/freezer300.e953d4a1.jpeg"
+  },
+  {
+    "revision": "fe352e36139ca2d1499896c20ed4bd92",
+    "url": "/Al-Madina/static/media/fruit.fe352e36.jpeg"
+  },
+  {
+    "revision": "9cbf962c74639762077dcdccd1b914c0",
+    "url": "/Al-Madina/static/media/good.9cbf962c.otf"
+  },
+  {
+    "revision": "5cb0b5e4b3dcb147e3c3281c55212305",
+    "url": "/Al-Madina/static/media/hardLogo.5cb0b5e4.jpg"
+  },
+  {
+    "revision": "0f7075d3a4a6b8ef40effb97d2c6d457",
+    "url": "/Al-Madina/static/media/homog.0f7075d3.jpeg"
+  },
+  {
+    "revision": "a696d5758a387004b463578b5734c42a",
+    "url": "/Al-Madina/static/media/ice2.a696d575.jpg"
+  },
+  {
+    "revision": "ab18749d2763ed33aaae602871af7be1",
+    "url": "/Al-Madina/static/media/iceLogo.ab18749d.jpg"
+  },
+  {
+    "revision": "db846ad541feaa90a1aca78b183ea802",
+    "url": "/Al-Madina/static/media/imag.db846ad5.jpeg"
+  },
+  {
+    "revision": "a438bdf71b4581c0dc52e04239fb214a",
+    "url": "/Al-Madina/static/media/importer.a438bdf7.png"
+  },
+  {
+    "revision": "b115cb21add0ce62df3a6d4ba8150f72",
+    "url": "/Al-Madina/static/media/logoo.b115cb21.jpg"
+  },
+  {
+    "revision": "36814c748978e0b31ecf007c66c7dcf5",
+    "url": "/Al-Madina/static/media/menu.36814c74.jpg"
+  },
+  {
+    "revision": "cb04570a6cff29d07fac65cb17d0106e",
+    "url": "/Al-Madina/static/media/npm.cb04570a.png"
+  },
+  {
+    "revision": "cf3936fe7759d816fcbd270fc8f29928",
+    "url": "/Al-Madina/static/media/slide1.cf3936fe.jpg"
+  },
+  {
+    "revision": "222425d118e5ed5466ccfbe0ec2cf316",
+    "url": "/Al-Madina/static/media/slide2.222425d1.jpg"
+  },
+  {
+    "revision": "2d337b3ffe4d5bab2e0a8db04043f312",
+    "url": "/Al-Madina/static/media/slide3.2d337b3f.jpg"
+  },
+  {
+    "revision": "2768551da1180077958b615f1cd55068",
+    "url": "/Al-Madina/static/media/twitter.2768551d.png"
+  }
+]);
