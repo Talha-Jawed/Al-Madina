7e14a3f882e2b8047f1596e578fdab9fc6257ac5
Talha-Jawed <jawedtalha0@gmail.com>
2020-06-22 10:51:10 +0500

    footer link buttons

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import devTo from '../assets/devTo.png';
+import linkedIn from '../assets/LinkedIn.png';
+import fb from '../assets/fb.png';
+import github from '../assets/github.png';
+import gmail from '../assets/gmail.png';
+import twitter from '../assets/twitter.png';
 import './Style.css';
 
 class Contact extends Component {
@@ -19,8 +25,19 @@ class Contact extends Component {
                     <p className='contact'>{'Contact: '}<span style={{ color: '#266cde' }}>+92 349 2562818</span> </p>
                     <p className='contact'>{'Email: '}<span style={{ color: '#266cde' }}>mohammadsaud296@gmail.com</span> </p>
                 </div>
-                <em className='made'>Made with ❤️ by Talha Javed</em>
-
+                
+                <div className="social-media-div">
+                <a href='https://www.linkedin.com/in/muhammad-saud-9636a4171/' target="_blank" rel="noopener noreferrer">
+                        <img src={linkedIn} alt="linkedIn" className='socialIcon' />
+                    </a>
+                    <a href="https://www.facebook.com/people/MUhammad-SaUd/100008157821981" target="_blank" rel="noopener noreferrer">
+                        <img src={fb} alt="fb" className='socialIcon' />
+                    </a>
+                    <a href="mailto:mohammadsaud296@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <img src={gmail} alt="npm" className='socialIcon' />
+                    </a>
+            </div>
+            <em className='made'>Made with ❤️ by Talha Javed</em>
             </div>
         );
     }
